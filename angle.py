332644f41f2a51262e137420422f3775259936b9
bernardoDev <bernardo.acaldas@gmail.com>
2021-06-30 21:30:49 -0300

    update

--- angle.py
+++ angle.py
@@ -2,9 +2,21 @@ import cv2
 import math
 
 image = cv2.imread('angles.png')
+pointsList = []
 
-cv2.imshow('imagem', image)
-c = cv2.waitKey(0)
-if c == 27:
-    cv2.destroyAllWindows()
+def mouseClick(event,x,y,flags,params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(image,(x,y), 5, (0,0,255),cv2.FILLED)
+        pointsList.append([x,y])
+        print(pointsList)
+    
+while True:
 
+    cv2.imshow('imagem', image)
+    cv2.setMouseCallback('imagem', mouseClick)
+    c = cv2.waitKey(1)
+    if c == 27:
+        break
+        
+
+cv2.destroyAllWindows()
