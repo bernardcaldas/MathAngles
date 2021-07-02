d20f8d06c4d5777d15e5869dcf7b214393f0faf9
bernardoDev <bernardo.acaldas@gmail.com>
2021-07-01 21:58:51 -0300

    update

--- angle.py
+++ angle.py
@@ -14,7 +14,7 @@ def getAngle(a,b,c):
     ang = math.degrees(math.atan2(c[1]-b[1], c[0]-b[0],) - math.atan2(a[1]-b[1], a[0]-b[0]))
 
     if ang < 0:
-        #ang + 360
+        ang + 360
         print(f'the value of the angle is {ang}, + 360 in this case')
     else:
         print(f'the value of the angle is {ang}.')    
@@ -25,6 +25,8 @@ while True:
     if len(pointsList) % 3 == 0 and len(pointsList) != 0:
         print( 'there are 3 elements')
         a,b,c = pointsList
+        cv2.line(image, a,b,(0,0,0),1,cv2.LINE_AA )
+        cv2.line(image, b,c,(0,0,0),1,cv2.LINE_AA )
         pointsList.clear()
         getAngle(a,b,c)
 
